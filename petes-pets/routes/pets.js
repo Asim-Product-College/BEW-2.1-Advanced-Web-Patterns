0a4eb82be295f09529afccdca27cb05687afa03b
Asim Zaidi <asimzaidih@gmail.com>
2019-01-28 14:26:37 -0800

    search paginated

--- petes-pets/routes/pets.js
+++ petes-pets/routes/pets.js
@@ -4,66 +4,75 @@ const Pet = require('../models/pet');
 // PET ROUTES
 module.exports = (app) => {
 
-  // INDEX PET => index.js
 
-  // NEW PET
-  app.get('/pets/new', (req, res) => {
-    res.render('pets-new');
+// NEW PET
+app.get('/pets/new', (req, res) => {
+  res.render('pets-new');
+});
+
+// CREATE PET
+app.post('/pets', (req, res) => {
+  var pet = new Pet(req.body);
+
+  pet.save()
+    .then((pet) => {
+      res.redirect(`/pets/${pet._id}`);
+    })
+    .catch((err) => {
+      // Handle Errors
+    }) ;
+});
+
+// SHOW PET
+app.get('/pets/:id', (req, res) => {
+  Pet.findById(req.params.id).exec((err, pet) => {
+    res.render('pets-show', { pet: pet });
   });
+});
 
-  // CREATE PET
-  app.post('/pets', (req, res) => {
-    var pet = new Pet(req.body);
-
-    pet.save()
-      .then((pet) => {
-        res.redirect(`/pets/${pet._id}`);
-      })
-      .catch((err) => {
-        // Handle Errors
-      }) ;
+// EDIT PET
+app.get('/pets/:id/edit', (req, res) => {
+  Pet.findById(req.params.id).exec((err, pet) => {
+    res.render('pets-edit', { pet: pet });
   });
+});
 
-  // SHOW PET
-  app.get('/pets/:id', (req, res) => {
-    Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-show', { pet: pet });
+// UPDATE PET
+app.put('/pets/:id', (req, res) => {
+  Pet.findByIdAndUpdate(req.params.id, req.body)
+    .then((pet) => {
+      res.redirect(`/pets/${pet._id}`)
+    })
+    .catch((err) => {
+      // Handle Errors
+      console.log(err);
     });
-  });
-
-  // EDIT PET
-  app.get('/pets/:id/edit', (req, res) => {
-    Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-edit', { pet: pet });
-    });
-  });
-
-  // UPDATE PET
-  app.put('/pets/:id', (req, res) => {
-    Pet.findByIdAndUpdate(req.params.id, req.body)
-      .then((pet) => {
-        res.redirect(`/pets/${pet._id}`)
-      })
-      .catch((err) => {
-        // Handle Errors
-        console.log(err);
-      });
-  });
+});
 
   // DELETE PET
-  app.delete('/pets/:id', (req, res) => {
-    Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
-      return res.redirect('/')
-    });
+app.delete('/pets/:id', (req, res) => {
+  Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
+    return res.redirect('/')
   });
+});
 
-  // SEARCH PET
-  app.get('/search', (req, res) => {
-    const term = new RegExp(req.query.term, 'i');
+// SEARCH PET
+app.get('/search', (req, res) => {
 
-    Pet.find({$or:[{'name': term}, {'species': term}]}).exec((err, pets) => {
-      res.render('pets-index', { pets: pets });
+  const term = new RegExp(req.query.term, 'i')
+
+  const page = req.query.page || 1
+  Pet.paginate(
+    {
+      $or: [
+        { 'name': term },
+        { 'species': term }
+      ]
+    },
+    { page: page }).then((results) => {
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term  });
     });
-  });
+});
 
-}
+
+} // end of of module exports
